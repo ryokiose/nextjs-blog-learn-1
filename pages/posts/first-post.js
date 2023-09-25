40787658cd3495888559ebf82b0218d6951332d2
manato miura <mmiura2351@gmail.com>
2023-09-25 09:31:24 +0900

    complete script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -6,6 +6,7 @@ export default function FirstPost() {
 
 import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
@@ -13,6 +14,13 @@ export default function FirstPost() {
       <Head>
         <title>Fist Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
