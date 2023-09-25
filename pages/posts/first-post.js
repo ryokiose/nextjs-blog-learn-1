9f6ac1984fd31b8285395203e98833034526b2f0
manato miura <mmiura2351@gmail.com>
2023-09-25 12:30:27 +0900

    global css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -7,10 +7,11 @@ export default function FirstPost() {
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Fist Post</title>
       </Head>
@@ -25,6 +26,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
