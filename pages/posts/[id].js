2eedd53c9d2e49cd141106221efe5656ec0d7c90
manato miura <mmiura2351@gmail.com>
2023-09-29 09:06:35 +0900

    Moved the POST function up.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,26 @@ import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>
+          {postData.title}
+        </title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingx1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
@@ -23,24 +43,4 @@ export async function getStaticProps({ params }) {
       postData,
     },
   };
-}
-
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      <Head>
-        <title>
-          {postData.title}
-        </title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingx1}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
-  );
 }
