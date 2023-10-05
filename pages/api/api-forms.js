363acc8974e222024a4ac654fa9152dc1f64100b
manato miura <mmiura2351@gmail.com>
2023-10-05 13:13:47 +0900

    added user data to db

--- pages/api/api-forms.js
+++ pages/api/api-forms.js
@@ -1,6 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+async function Post(userName, userEmail) {
+  const prisma = new PrismaClient();
+  const newPost = await prisma.Post.create({
+    data: {
+      name: userName,
+      email: userEmail,
+    },
+  });
+}
+
 export default function handler(req, res) {
   const userName = req.body.name;
   const userEmail = req.body.email;
-  console.log(userName, userEmail);
+  Post(userName, userEmail);
   res.status(200).json({ name: userName, email: userEmail })
 }
