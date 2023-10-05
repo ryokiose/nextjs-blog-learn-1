4cfd652035f68b3db924118bb8591f9a75828bee
manato miura <mmiura2351@gmail.com>
2023-10-05 13:12:30 +0900

    migration

---
+++ prisma/migrations/20231005040242_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
