1586c68900d73303357f88366e5f27eee1421917
manato miura <mmiura2351@gmail.com>
2023-10-17 09:17:57 +0900

    Add loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import "@/src/styles/global.css";
 import { AppProps } from "next/app";
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-}
+nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 });
+
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+	if (typeof window !== "undefined") {
+		nprogress.start();
+	}
+
+	useEffect(() => {
+		nprogress.done();
+	});
+	return <Component {...pageProps} />;
+};
 
 export default App;
