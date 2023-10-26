b0bc7d6571db7245f2c8fd1eb1257839cfd6e5b1
manato miura <mmiura2351@gmail.com>
2023-10-26 15:24:30 +0900

    create login

---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,56 @@
+import styles from './index.module.css'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from 'react';
+import Router from 'next/router';
+import Link from 'next/link';
+
+import { firebaseApp } from '@/src/lib/firebase/firebaseConfig';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSetEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }
+
+  const handleSetPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }
+
+  const submit = () => {
+    const auth = getAuth(firebaseApp);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        alert( 'ログイン完了' );
+        console.log( user );
+        Router.push("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <>
+      <div className={styles.form}>
+        <h1>ログイン</h1>
+        <input type="text"
+          placeholder="メールアドレス"
+          value={email}
+          onChange = {(e) => handleSetEmail(e)}
+        />
+        <input type="password"
+          placeholder="パスワード"
+          value={password}
+          onChange = {(e) => handleSetPassword(e)}
+        />
+        <button onClick={submit}>ログイン</button>
+      </div>
+    </>
+  )
+}
+
+export default Login;
