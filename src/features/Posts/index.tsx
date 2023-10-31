79c2fbfe7b051883d4c82ce1278bb2345fec7b69
MiuraManato <manato20030510@icloud.com>
2023-10-31 18:29:47 +0900

    move to features

---
+++ src/features/Posts/index.tsx
@@ -0,0 +1,24 @@
+import Layout from "@/components/Layout";
+import { getAllPostIds, getPostData } from "@/utils/posts";
+import Head from "next/head";
+import Date from "@/components/Elements/Date/date";
+import utilStyles from "@/styles/utils.module.css";
+import { Params, PostData } from "@/types/src/posts/[id]";
+
+export const Post = ({ postData }: { postData: PostData }) => {
+	return (
+		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			<article>
+				<h1 className={utilStyles.headingx1}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<br />
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
+		</Layout>
+	);
+};
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,27 +1,10 @@
-import Layout from "@/components/Layout";
-import { getAllPostIds, getPostData } from "@/utils/posts";
-import Head from "next/head";
-import Date from "@/components/Elements/Date/date";
-import utilStyles from "@/styles/utils.module.css";
 import { Params, PostData } from "@/types/src/posts/[id]";
+import { getAllPostIds, getPostData } from "@/utils/posts";
+import { Post } from "@/features/Posts"
 
-const Post = ({ postData }: { postData: PostData }) => {
-	return (
-		<Layout>
-			<Head>
-				<title>{postData.title}</title>
-			</Head>
-			<article>
-				<h1 className={utilStyles.headingx1}>{postData.title}</h1>
-				<div className={utilStyles.lightText}>
-					<Date dateString={postData.date} />
-				</div>
-				<br />
-				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-			</article>
-		</Layout>
-	);
-};
+export default function PostPage({ postData }: { postData: PostData }) {
+	return <Post postData={postData} />;
+}
 
 export async function getStaticPaths() {
 	const paths = getAllPostIds();
@@ -39,5 +22,3 @@ export async function getStaticProps({ params }: Params) {
 		},
 	};
 }
-
-export default Post;
