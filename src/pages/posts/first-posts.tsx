059982a2fc02814fa57efc8ef2fb47dcd1a61219
manato miura <mmiura2351@gmail.com>
2023-11-07 11:27:57 +0900

    feat: Add back to home link

--- src/pages/posts/first-posts.tsx
+++ src/pages/posts/first-posts.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const FirstPost = () => {
-	return <h1>First Post</h1>;
+	return (
+		<>
+			<h1>First Post</h1>
+			<Link href={"/"}>Back to Home</Link>
+		</>
+	);
 };
 
 export default FirstPost;
